519006f7c5ec069a28814934b83115cc2b51a193
Rohan Yadav <83074738+rohany395@users.noreply.github.com>
2025-09-04 18:57:20 +0000

    update layouts

--- lab1.py
+++ lab1.py
@@ -5,7 +5,6 @@ from openai import OpenAI
 st.title("📄 Rohan's Document question answering app")
 st.write(
     "Upload a document below and ask a question about it – GPT will answer! "
-    "To use this app, you need to provide an OpenAI API key, which you can get [here](https://platform.openai.com/account/api-keys). "
 )
 
 # Ask user for their OpenAI API key via `st.text_input`.
--- lab2.py
+++ lab2.py
@@ -1,16 +1,15 @@
 import streamlit as st
 from openai import OpenAI
 
+#sidebar
+add_sidebar=st.sidebar.selectbox("Summary options",("Summarize the document in 100 words","Summarize the document in 2 connecting paragraphs","Summarize the document in 5 bullet points"))
+
 # Show title and description.
 st.title("📄 Rohan's Document question answering app")
 st.write(
     "Upload a document below and ask a question about it – GPT will answer! "
-    "To use this app, you need to provide an OpenAI API key, which you can get [here](https://platform.openai.com/account/api-keys). "
 )
 
-# Ask user for their OpenAI API key via `st.text_input`.
-# Alternatively, you can store the API key in `./.streamlit/secrets.toml` and access it
-# via `st.secrets`, see https://docs.streamlit.io/develop/concepts/connections/secrets-management
 openai_api_key = st.secrets["API_KEY"]
 if not openai_api_key:
     st.info("Please add your OpenAI API key to continue.", icon="🗝️")
@@ -24,27 +23,36 @@ else:
         "Upload a document (.txt or .md)", type=("txt", "md")
     )
 
-    # Ask the user for a question via `st.text_area`.
-    question = st.text_area(
-        "Now ask a question about the document!",
-        placeholder="Can you give me a short summary?",
-        disabled=not uploaded_file,
-    )
+    #checkbox
+    model=st.radio("Model",("Mini","Advanced Model"))
+    gptVersion="gpt-5"
 
-    if uploaded_file and question:
+    if model=="Mini":
+        gptVersion="gpt-4o-mini"
+    else:
+        gptVersion="gpt-4o"
+
+    # # Ask the user for a question via `st.text_area`.
+    # question = st.text_area(
+    #     "Now ask a question about the document!",
+    #     placeholder="Can you give me a short summary?",
+    #     disabled=not uploaded_file,
+    # )
+
+    if uploaded_file and add_sidebar:
 
         # Process the uploaded file and question.
         document = uploaded_file.read().decode()
         messages = [
             {
                 "role": "user",
-                "content": f"Here's a document: {document} \n\n---\n\n {question}",
+                "content": f"Here's a document: {document} \n\n---\n\n {add_sidebar}",
             }
         ]
 
         # Generate an answer using the OpenAI API.
         stream = client.chat.completions.create(
-            model="gpt-3.5-turbo",
+            model=gptVersion,
             messages=messages,
             stream=True,
         )
